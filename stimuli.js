395038beb7d40db2cdf0c0f43ef4ca9d46556b67
Kyle Kurkela <kyle.kurkela@healogix.com>
2024-07-22 09:19:34 -0400

    added a center img

--- stimuli.js
+++ stimuli.js
@@ -1,5 +1,5 @@
 var stim_list = [
-    {stim1: 'img/blue.png', stim2: 'img/orange.png'},
-    {stim1: 'img/orange.png', stim2: 'img/blue.png'},
-    {stim1: 'img/blue.png',   stim2: 'img/orange.png'},
+    {stim1: 'img/blue.png', stim2: 'img/orange.png', stimCenter: 'img/blue.png'},
+    {stim1: 'img/orange.png', stim2: 'img/blue.png', stimCenter: 'img/orange.png'},
+    {stim1: 'img/blue.png',   stim2: 'img/orange.png', stimCenter: 'img/orange.png'},
   ]
