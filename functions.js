c2951dff3381f4fcfa223e1ad7f49b13fba09add
Kyle Kurkela <kyle.kurkela@healogix.com>
2024-07-24 12:27:24 -0400

    revamped demo script

--- functions.js
+++ functions.js
@@ -1,9 +1,11 @@
-function construct_button() {
+/* Functions for constructing instructions stimuli */
+
+function construct_instr_buttn() {
     return `<button id=buttonStyle>${start_button_text}</button>`
 }
 
 function construct_prompt() {
-    return `<img id=centerStim src=${jsPsych.timelineVariable('stimCenter')}><p id=promptStyle>${prompt_text}</p>`
+    return `<p id=promptStyle>${prompt_text}</p>`
 }
 
 function construct_stim_one() {
@@ -14,6 +16,40 @@ function construct_stim_two() {
     return `<img id=topRightStim src="${jsPsych.timelineVariable('stim2')}">`
 }
 
-function construct_both() {
-    return construct_stim_one() + construct_stim_two()
+function construct_trial_stim(){
+    return `<img id=StimCenter src=${jsPsych.timelineVariable('stimCenter')}></img>`
 }
+
+function construct_instr_stim() {
+    return construct_stim_one() + construct_stim_two() + construct_prompt()
+}
+
+/* Functions for constructing trial stimuli */
+
+function construct_trial_bttns(x) {
+
+    if(jsPsych.timelineVariable('stim1') == x){
+        return construct_stim_one()
+    } else if(jsPsych.timelineVariable('stim2') == x){
+        return construct_stim_two()
+    }
+
+}
+
+function build_mousetrack_fig() {
+
+    var mouse_tracking_data = jsPsych.data.get().select('mouse_tracking_data').values;
+    var trace = mouse_tracking_data.map((j, idx) => {
+      var x = j.map((k) => k.x)
+      x = x.map((t) => t - x[0])
+      var y = j.map((k) => k.y)
+      y = y.map((t) => (t - y[0]) * -1 )
+      return {x:x, y:y, type: 'scatter', name: `trial ${idx+1}`}
+    })
+
+    content = document.getElementById('jspsych-content');
+    Plotly.newPlot(content, trace);
+
+    jsPsych.data.get().localSave('csv','mydata.csv');
+
+  }
--- parameters.js
+++ parameters.js
@@ -7,7 +7,7 @@
 */
 
 // prompt
-var prompt_text         = 'Is the circle above blue or orange? Please select your answer with your mouse. Press Start to Begin.';
+var prompt_text         = 'Is the image a fruit or a vegetable? Please click on your response as quickly and accurately as you can. Press start to begin trial.';
 
 // button
 var start_button_text   = 'Start';
