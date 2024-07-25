cd7896797c2a4134bdf1366f94faa5fd32925535
Kyle Kurkela <kyle.kurkela@healogix.com>
2024-07-25 11:03:49 -0400

    adding index.js to see how cognition integrates with github

---
+++ index.js
@@ -0,0 +1,48 @@
+// initialze jsPsych
+var jsPsych = initJsPsych({
+    on_finish: build_mousetrack_fig,
+    extensions: [
+      { type: jsPsychExtensionMouseTracking, params: {minimum_sample_time: 0} }
+    ]
+  });
+  
+  // instructions
+  var instruct = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: construct_instr_stim,
+    choices: ['Start'],
+    button_html: construct_instr_buttn,
+    prompt: null,
+    data: {
+      task: 'instructions'
+    },
+  };
+  
+  // trial
+  var trial = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: construct_trial_stim,
+    choices: [jsPsych.timelineVariable('stim1'), jsPsych.timelineVariable('stim2')],
+    prompt: null,
+    button_html: construct_trial_bttns,
+    extensions: [
+      {type: jsPsychExtensionMouseTracking, params: {targets: []}}
+    ],  
+  }
+  
+  // fixations
+  var fixation = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: '',
+    choices: "NO_KEYS",
+    trial_duration: 1,
+  }
+  
+  // experimental procedure
+  mouse_track_procedure = {
+    timeline: [instruct, fixation, trial, fixation],
+    timeline_variables: stim_list,
+  }
+  
+  // run the experiment
+  jsPsych.run([mouse_track_procedure]);
