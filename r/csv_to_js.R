4a9d0670f80de314dbb8b6ffa1cd2c8184b35719
Kyle Kurkela <kyle.kurkela@healogix.com>
2024-07-31 11:18:47 -0400

    added sheet spec to csv_to_js R script

--- r/csv_to_js.R
+++ r/csv_to_js.R
@@ -10,7 +10,7 @@ csv_file  <- file.choose()
 outputDir <- choose.dir(caption = 'Choose Folder to Write JavaScript File to.')
 
 # read in the excel file as a tbl
-tbl       <- read_excel(path = csv_file)
+tbl       <- read_excel(path = csv_file, sheet = 'Sheet1')
 
 # create the javascript formatted string
 text <- str_c('var stim_list = ', toJSON(tbl))
