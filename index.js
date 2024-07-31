344b726d792228a311e9dd1717c9ea8b9558e848
Kyle Kurkela <kyle.kurkela@healogix.com>
2024-07-31 08:23:39 -0400

    further cognition.run tweaking

--- index.js
+++ index.js
@@ -6,49 +6,67 @@ var jsPsych = initJsPsych({
     ]
   });
 
-  // preload
-  var preload = {
-    type: jsPsychPreload,
-    images: find_unique_stim(stim_list)
-  }
-  
-var instructions = {
-  type: jsPsychInstructions,
-  pages: ['Page 1', 'Page 2'],
-  buttom_label_next: "",
-  button_label_previous: "",
-  show_clicable_nav: true
+// preload stimuli
+// jsPsych module that preloads stimuli prior to the start of the experiment
+// ensures that there is no delay during trials
+var preload = {
+  type: jsPsychPreload,
+  images: find_unique_stim(stim_list)
 }
 
-  // instructions
-  var instruct = {
-    type: jsPsychHtmlButtonResponse,
-    stimulus: construct_instr_stim,
-    choices: ['Start'],
-    button_html: construct_instr_buttn,
-    prompt: null,
-    data: {
-      task: 'instructions'
-    },
-  };
-  
-  // trial
-  var trial = {
-    type: jsPsychHtmlButtonResponse,
-    stimulus: construct_trial_stim,
-    choices: [jsPsych.timelineVariable('stim1'), jsPsych.timelineVariable('stim2')],
-    prompt: null,
-    button_html: construct_trial_bttns,
-    extensions: [
-      {type: jsPsychExtensionMouseTracking, params: {targets: []}}
-    ],  
-  }
+// enter full screen mode
+// forces the participant to enter full screen mode to complete the experiment
+var fullscreen = {
+  type: jsPsychFullscreen,
+  fullscreen_mode: true,
+  stimulus: '<p>The experiment will switch to fullscreen mode when you press the button below</p>',
+  choices: ['Continue'],
+  delay_after: 1000
+}
 
-  // experimental procedure
-  mouse_track_procedure = {
-    timeline: [instruct, trial],
-    timeline_variables: stim_list,
+// Navigateable participant instructions
+var instructions = {
+  type: jsPsychInstructions,
+  pages: instructions_pages,
+  buttom_label_next: button_label_next,
+  button_label_previous: button_label_previous,
+  show_clicable_nav: true,
+  show_page_number: show_page_number,
+  page_label: page_label,
+  data: {
+    task: 'instructions'
   }
-  
-  // run the experiment
-  jsPsych.run([preload, instructions, mouse_track_procedure]);
+}
+
+// trial instructions
+var trial_instrct = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: construct_instr_stim,
+  choices: ['Start'],
+  button_html: construct_instr_buttn,
+  prompt: null,
+  data: {
+    task: 'trial_instructions'
+  },
+};
+
+// trial
+var trial = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: construct_trial_stim,
+  choices: [jsPsych.timelineVariable('stim1'), jsPsych.timelineVariable('stim2')],
+  prompt: null,
+  button_html: construct_trial_bttns,
+  extensions: [
+    {type: jsPsychExtensionMouseTracking, params: {targets: []}}
+  ],  
+}
+
+// experimental procedure
+mouse_track_procedure = {
+  timeline: [trial_instrct, trial],
+  timeline_variables: stim_list,
+}
+
+// run the experiment
+jsPsych.run([preload, fullscreen, instructions, mouse_track_procedure]);
--- parameters.js
+++ parameters.js
@@ -6,6 +6,13 @@
 
 */
 
+// instructions
+var instructions_pages    = ['Page 1', 'Page 2'];
+var button_label_next     = "Next";
+var button_label_previous = "Previous";
+var show_page_number      = true;
+var page_label            = 'Page';
+
 // prompt
 var prompt_text         = 'Is the image a fruit or a vegetable? Please click on your response as quickly and accurately as you can. Press start to begin trial.';
 
@@ -16,4 +23,3 @@ var start_button_text   = 'Start';
 var trial_duration      = null;
 var response_ends_trial = true;
 var time_after_response = null;
-
